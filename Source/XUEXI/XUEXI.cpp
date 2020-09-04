394f141d42f873beb56a2a7b342bc516dc521782
WHITOPE <1506407791@qq.com>
2020-09-04 22:42:10 +0800

    init

---
+++ Source/XUEXI/XUEXI.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "XUEXI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, XUEXI, "XUEXI" );
+ 
---
+++ Source/XUEXI/XUEXIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "XUEXIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AXUEXIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AXUEXIGameMode();
+};
+
+
+
